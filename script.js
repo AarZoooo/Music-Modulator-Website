e12d6166c797f2223294e0975f233165bcba948d
Aarju Pal <aarju.pal1234@gmail.com>
2023-05-05 19:11:44 +0530

    changes

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 // get the audio player iframe
-const audioPlayer = document.getElementById('audio-player');
+const audioPlayer = document.getElementById('audio-player').querySelector('iframe');
+
+// initialize SoundCloud widget
+const widget = SC.Widget(audioPlayer);
 
 // get the volume slider and set the initial value to 50
 const volumeSlider = document.getElementById('volume-slider');
-audioPlayer.contentWindow.postMessage('{"type": "volume", "data": ' + volumeSlider.value / 100 + '}', '*');
-
-// add event listener to volume slider
+widget.setVolume(volumeSlider.value / 100);
 volumeSlider.addEventListener('input', function() {
-  audioPlayer.contentWindow.postMessage('{"type": "volume", "data": ' + this.value / 100 + '}', '*');
+  widget.setVolume(this.value / 100);
 });
 
 // get the frequency slider and set the initial value to 50
 const frequencySlider = document.getElementById('frequency-slider');
-audioPlayer.contentWindow.postMessage('{"type": "seek", "data": audioPlayer.contentWindow.SC.Widget.Events.SEEK + ': ' + audioPlayer.contentWindow.SC.Widget.Events.PLAY_PROGRESS + ': ' + (frequencySlider.value / 100) * audioPlayer.contentWindow.document.querySelector('.sound__waveform svg g').getTotalLength() + '}', '*');
-
-// add event listener to frequency slider
+widget.bind(SC.Widget.Events.READY, function() {
+  widget.unbind(SC.Widget.Events.READY);
+  widget.bind(SC.Widget.Events.PLAY_PROGRESS, function() {
+    const totalTime = this.getDurationEstimate();
+    const currentPosition = this.getPosition();
+    const waveformLength = this.getCurrentSound().waveform_url.match(/-([0-9]+)\./)[1];
+    const newSeekPosition = (currentPosition / totalTime) * waveformLength * (frequencySlider.value / 100);
+    widget.seekTo(newSeekPosition);
+  });
+});
 frequencySlider.addEventListener('input', function() {
-  audioPlayer.contentWindow.postMessage('{"type": "seek", "data": audioPlayer.contentWindow.SC.Widget.Events.SEEK + ': ' + audioPlayer.contentWindow.SC.Widget.Events.PLAY_PROGRESS + ': ' + (this.value / 100) * audioPlayer.contentWindow.document.querySelector('.sound__waveform svg g').getTotalLength() + '}', '*');
+  const totalTime = widget.getDurationEstimate();
+  const currentPosition = widget.getPosition();
+  const waveformLength = widget.getCurrentSound().waveform_url.match(/-([0-9]+)\./)[1];
+  const newSeekPosition = (currentPosition / totalTime) * waveformLength * (this.value / 100);
+  widget.seekTo(newSeekPosition);
 });
