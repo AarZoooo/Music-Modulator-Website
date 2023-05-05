ee848f85f99ca76379142e72552b0ad0a6d76948
Aarju Pal <aarju.pal1234@gmail.com>
2023-05-05 21:23:48 +0530

    final version

--- script.js
+++ script.js
@@ -1,36 +1,26 @@
-// Get the audio element and all the sliders
 const audio = document.querySelector('audio');
-const volumeSlider = document.querySelector('#volume-slider');
-const speedSlider = document.querySelector('#speed-slider');
-const pitchSlider = document.querySelector('#pitch-slider');
-const resetButton = document.querySelector('#reset-button');
+const volumeSlider = document.getElementById("volumeSlider");
+const speedSlider = document.getElementById("speedSlider");
+const resetButton = document.getElementById("resetButton");
 
-// Set the initial values of the sliders
-volumeSlider.value = 50;
-speedSlider.value = 100;
-pitchSlider.value = 0;
+// Add event listeners to the sliders
+volumeSlider.addEventListener("input", updateVolume);
+speedSlider.addEventListener("input", updateSpeed);
 
-// Add event listeners to all the sliders
-volumeSlider.addEventListener('input', () => {
-	const volumeValue = volumeSlider.value;
-	audio.volume = volumeValue / 100;
-});
+// Add event listener to the reset button
+resetButton.addEventListener("click", resetSliders);
 
-speedSlider.addEventListener('input', () => {
-	const speedValue = speedSlider.value;
-	audio.playbackRate = speedValue / 100;
-});
+function updateVolume() {
+  audio.volume = volumeSlider.value / 100;
+}
 
-pitchSlider.addEventListener('input', () => {
-	const pitchValue = pitchSlider.value;
-	const cents = 1200 * Math.log2(Math.pow(2, pitchValue / 12));
-	audio.playbackRate = Math.pow(2, cents / 1200);
-});
+function updateSpeed() {
+  audio.playbackRate = speedSlider.value;
+}
 
-resetButton.addEventListener('click', () => {
-	volumeSlider.value = 50;
-	speedSlider.value = 100;
-	pitchSlider.value = 0;
-	audio.volume = 0.5;
-	audio.playbackRate = 1;
-});
+function resetSliders() {
+  volumeSlider.value = 50;
+  speedSlider.value = 1;
+  audio.volume = 0.5;
+  audio.playbackRate = 1;
+}
