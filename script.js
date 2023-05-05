529220d553b48b4129adaf3fc6fbb8b029f02c17
Aarju Pal <aarju.pal1234@gmail.com>
2023-05-05 19:17:35 +0530

    volume working

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
-// get the audio player iframe
-const audioPlayer = document.getElementById('audio-player').querySelector('iframe');
+const audio = document.getElementById("audio");
+const volumeSlider = document.getElementById("volume-slider");
+const frequencySlider = document.getElementById("frequency-slider");
 
-// initialize SoundCloud widget
-const widget = SC.Widget(audioPlayer);
+// Set default values
+audio.volume = 0.5;
+let frequencyValue = 0.5;
 
-// get the volume slider and set the initial value to 50
-const volumeSlider = document.getElementById('volume-slider');
-widget.setVolume(volumeSlider.value / 100);
-volumeSlider.addEventListener('input', function() {
-  widget.setVolume(this.value / 100);
-});
+// Function to update the frequency value and apply the modulation effect
+function updateFrequency() {
+  frequencyValue = frequencySlider.value / 100;
+  const modulatedValue = Math.sin(audio.currentTime * frequencyValue * 2 * Math.PI) * 0.1;
+  const volumeValue = volumeSlider.value / 100;
+  audio.volume = volumeValue + modulatedValue;
+}
 
-// get the frequency slider and set the initial value to 50
-const frequencySlider = document.getElementById('frequency-slider');
-widget.bind(SC.Widget.Events.READY, function() {
-  widget.unbind(SC.Widget.Events.READY);
-  widget.bind(SC.Widget.Events.PLAY_PROGRESS, function() {
-    const totalTime = this.getDurationEstimate();
-    const currentPosition = this.getPosition();
-    const waveformLength = this.getCurrentSound().waveform_url.match(/-([0-9]+)\./)[1];
-    const newSeekPosition = (currentPosition / totalTime) * waveformLength * (frequencySlider.value / 100);
-    widget.seekTo(newSeekPosition);
-  });
-});
-frequencySlider.addEventListener('input', function() {
-  const totalTime = widget.getDurationEstimate();
-  const currentPosition = widget.getPosition();
-  const waveformLength = widget.getCurrentSound().waveform_url.match(/-([0-9]+)\./)[1];
-  const newSeekPosition = (currentPosition / totalTime) * waveformLength * (this.value / 100);
-  widget.seekTo(newSeekPosition);
-});
+// Attach event listeners to the sliders
+volumeSlider.addEventListener("input", updateFrequency);
+frequencySlider.addEventListener("input", updateFrequency);
